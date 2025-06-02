d68ac7e27488e487f910c8636f68fb35f8666b8a
Douglas Bernardo <douglasbernardomelo@gmail.com>
2025-06-01 23:27:17 -0300

    mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/frontend-fusion'),
+    MongooseModule.forRoot('mongodb+srv://douglasmelo:api-fusion@cluster0.bbe3uaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
     PlanetsModule,
     CharacterModule,
     starSystemModule,
